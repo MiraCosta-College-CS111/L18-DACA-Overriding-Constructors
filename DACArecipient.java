d1377c36373b681f3db6b66454a6fd00e92601ef
Stephen Cerruti <scerruti@miracosta.edu>
2024-07-22 10:55:34 -0700

    Solution
    Converted from replit.

--- DACArecipient.java
+++ DACArecipient.java
@@ -1,12 +1,10 @@
 /**
- * Represents one person receiving deferred action for childhood arrivals (DACA).
+ * Represents one person
  * 
  * <TODO add @author info here for all group-mates!>
- *
- * @version 1.2
  **/
 
-//TODO: Complete UML class diagram
+//TODO PART 2: Complete UML class diagram
 /* UML CLASS DIAGRAM:
 -----------------------------------------
 					DACArecipient
@@ -20,6 +18,8 @@
 - expirationDate : int
 - sex : char
 -----------------------------------------
++ DACArecipient()
++ DACArecipient(surname:String, givenName:String, uscisNumber:String, countryOfOrigin:String, birthday:int, validFromDate:int, expirationDate:int,sex:char)
 + getSurname() : String
 + getGivenName() : String
 + getUscisNumber() : String
@@ -47,6 +47,7 @@
 public class DACArecipient
 {
 	/***** INSTANCE VARIABLES *****/
+	//DONE: Declare the instance variables. 
   private String surname;
   private String givenName;
   private String uscisNumber;
@@ -54,14 +55,32 @@ public class DACArecipient
   private int birthday, validFromDate, expirationDate;
   private char sex;
 	
-
-	//TODO: don't forget to copy all your methods from the previous labs!
-	
   /**** CONSTRUCTORS ****/
-  //TODO: Write the no-argument constructor and full constructor. Remember to include documentation for each method.
+  //DONE: Write the default constructor and constructor with parameters. Remember to include documentation for each method.
+  public DACArecipient()
+  {
+    surname = "Unknown";
+    givenName = "Unknown";
+    uscisNumber = "Unknown";
+    countryOfOrigin = "Unknown";
+    birthday = 2415021;
+    validFromDate = 2415021;
+    expirationDate = 2415021;
+    sex = 'X';
+  }
 
-
-	/***** OTHER REQUIRED METHODS *****/
+  public DACArecipient(String surname, String givenName, String uscisNumber, String countryOfOrigin, int birthday, int validFromDate, int expirationDate, char sex)
+  {
+    this.surname = surname;
+    this.givenName = givenName;
+    this.uscisNumber = uscisNumber;
+    this.countryOfOrigin = countryOfOrigin;
+    this.birthday = birthday;
+    this.validFromDate = validFromDate;
+    this.expirationDate = expirationDate;
+    this.sex = sex;
+  }
+  
   public String toString()
   {
     return "Surname: " + surname + ", Given Name: " + givenName + ", USCIS Number: " + uscisNumber + ", Country of Origin: " + countryOfOrigin + ", Birthday: " + birthday + ", Valid From Date: " + validFromDate + ", Expiration Date: " + expirationDate + ", Sex: " + sex;
--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-/********************************************
-*	DACArecipient Tester
-*********************************************
-*	PROGRAM DESCRIPTION:
-*	This program is to test methods of the DACArecipient class.
-*********************************************/
 class Main 
 {
   public static void main(String[] args) 
-	{
+  {
+    DACArecipient person1 = new DACArecipient();
+    DACArecipient person2 = new DACArecipient("Hernandez", "Alex", "25-6-567", "Mexico", 2451257, 39567322, 39567818, 'F');
 
+    System.out.println(person1 + "\n\n" + person2);
   }
 }
